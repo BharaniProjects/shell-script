faa7748e8800518c7f0702e56dfd2feb886c21ce
BharaniProjects <bharani471@gmail.com>
2025-09-23 21:29:47 +0530

    made some changes

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-echo "suresh :: Hi,Ramesh Good morning."
-echo "ramesh :: Hi,suresh very Good Morning."
-echo "ramesh :: How are you suresh."
-echo "suresh :: I am fine ramesh."
+PERSON1=SURESH
+PERSON2=RAMESH
+echo "PERSON1 :: Hi,PERSON2 Good morning."
+echo "PERSON2 :: Hi,PERSON1 very Good Morning."
+echo "ramesh :: How are you PERSON1."
+echo "PERSON1 :: I am fine PERSON2."
