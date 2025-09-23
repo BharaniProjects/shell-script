2f72830d3572164f38021782b7cc30367b838183
BharaniProjects <bharani471@gmail.com>
2025-09-23 22:04:49 +0530

    created new file

---
+++ 06-arrays.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+FRUITS=("Apple","Mango","Banana")
+echo "The first fruit : ${FRUITS[0]}"
+echo "The second fruit : ${FRUITS[1]}"
+echo "The Third fruit  : ${FRUITS[2]}"
+
+echo "All fruits : ${FRUITS[@]}"
