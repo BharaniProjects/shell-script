94e51933b0a7aa8c9b190167ce7eaf724dd27f39
BharaniProjects <bharani471@gmail.com>
2025-09-23 22:16:45 +0530

    made some changes

--- 06-arrays.sh
+++ 06-arrays.sh
@@ -2,8 +2,7 @@
 
 FRUITS=("Apple","Mango","Banana")
 
-echo "The first value : ${$FRUITS[0]}"
-echo "The second value : ${$FRUITS[1]}"
-echo "The Third value  : ${$FRUITS[2]}"
-
-echo "All fruits : ${$FRUITS[@]}"
+echo "The first value is : ${FRUITS[0]}"
+echo "The second value is : ${FRUITS[1]}"
+echo "The Third value is : ${FRUITS[2]}"
+echo "The all fruits : ${FRUITS[@]}"
