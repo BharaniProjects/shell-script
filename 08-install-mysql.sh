6f7a43de69103a980ea3eea807cc0c0050fb966a
BharaniProjects <bharani471@gmail.com>
2025-09-24 15:02:44 +0530

    created new file

--- 08-install-mysql.sh
+++ 08-install-mysql.sh
@@ -5,7 +5,16 @@ ID=$(id -u)
 if [ $ID -ne 0 ]
 then  
     echo "ERROR :: Please access with root user."
+    exit 1
 else
     echo "You are a root user."
 fi
 dnf install mysql -y
+
+if [ $? -ne 0 ]
+then
+    echo "ERROR :: Installing mysql is failed."
+    exit 1
+else
+    echo "Installing mysql is success."
+fi
